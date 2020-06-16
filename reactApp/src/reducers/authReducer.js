c2c0d25abcd440796a847539822607da93684b85
saif43 <saif.ahmed.anik.0@gmail.com>
2020-06-16 21:21:50 +0600

    encrpting tokenID in redux

--- reactApp/src/reducers/authReducer.js
+++ reactApp/src/reducers/authReducer.js
@@ -6,7 +6,7 @@ import {
   SIGN_OUT,
   LOADING,
 } from "./../actions/types";
-import { encrypt, decrypt } from "../components/Security";
+import { encrypt } from "../components/Security";
 
 const INITIAL_STATE = {
   isSignedIn: false,
