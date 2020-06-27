036690d406aa935b1a0a9fb78548916cc6ebfbd0
saif43 <saif.ahmed.anik.0@gmail.com>
2020-06-27 14:19:26 +0600

    page connecting

--- reactApp/src/components/Events/EventItem.js
+++ reactApp/src/components/Events/EventItem.js
@@ -3,29 +3,51 @@ import { Link } from "react-router-dom";
 
 const EventItem = () => {
   return (
-    <div className="sidebar-menu">   
-                <button
-                  className="btn btn-lg btn-transparent dropdown-toggle"
-                  type="button"
-                  id="dropdownMenu2"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                <span className="sidebar_menu_text">New Project</span>
-                </button>
+    <div className="sidebar-menu">
+      <button
+        className="btn btn-lg btn-transparent dropdown-toggle"
+        type="button"
+        id="dropdownMenu2"
+        data-toggle="dropdown"
+        aria-haspopup="true"
+        aria-expanded="false"
+      >
+        <span className="sidebar_menu_text">New Project</span>
+      </button>
       <div className="dropdown-menu" aria-labelledby="dropdownMenu2">
-        <Link to="/create" className="btn btn-lg btn-transparent dropdown-item" href="#" role="button">Create Event
+        <Link
+          to="/event/create"
+          className="btn btn-lg btn-transparent dropdown-item"
+          href="#"
+          role="button"
+        >
+          Create Event
         </Link>
-        <Link to="/create" className="btn btn-lg btn-transparent dropdown-item" href="#" role="button">Create Design
+        <Link
+          to="/design"
+          className="btn btn-lg btn-transparent dropdown-item"
+          href="#"
+          role="button"
+        >
+          Create Design
         </Link>
-      </div>        
+      </div>
       <ul>
-        <li><Link to='/'>Project 1</Link></li>
-        <li><Link to='/'>Project 2</Link></li>
-        <li><Link to='/'>Project 3</Link></li>
-        <li><Link to='/'>Project 4</Link></li>
-        <li><Link to='/'>Project 5</Link></li>
+        <li>
+          <Link to="/">Project 1</Link>
+        </li>
+        <li>
+          <Link to="/">Project 2</Link>
+        </li>
+        <li>
+          <Link to="/">Project 3</Link>
+        </li>
+        <li>
+          <Link to="/">Project 4</Link>
+        </li>
+        <li>
+          <Link to="/">Project 5</Link>
+        </li>
       </ul>
     </div>
   );
--- reactApp/src/components/Homepage/Homepage.js
+++ reactApp/src/components/Homepage/Homepage.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import Landing from "./Landing";
 import Navbar from "./../template/Navbar";
+import Helmet from "react-helmet";
 
 export default class Homepage extends Component {
   render() {
     return (
       <div>
+        <Helmet>
+          <title>Home</title>
+        </Helmet>
         <Navbar />
         <Landing />
       </div>
